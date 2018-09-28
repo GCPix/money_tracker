65bf2a4ea3a29756cc1412fe91074a29d0ec8dfc
GillianC <gccc0309@gmail.com>
2018-09-28 16:06:02 +0100

    database created

---
+++ db/money_tracker.sql
@@ -0,0 +1,30 @@
+DROP TABLE transactions;
+DROP TABLE tags;
+DROP TABLE merchants;
+
+CREATE TABLE merchants
+(
+id SERIAL4 PRIMARY KEY,
+name VARCHAR(255)
+
+
+);
+
+CREATE TABLE tags(
+id SERIAL4 PRIMARY KEY,
+name VARCHAR(255)
+
+
+
+);
+
+CREATE TABLE transactions(
+id SERIAL4 PRIMARY KEY,
+merchant_id INT4 REFERENCES merchants(id),
+tag_id INT4 REFERENCES tags(id),
+type VARCHAR(255),
+description VARCHAR(255),
+amount INT2,
+transaction_date DATE
+
+);
