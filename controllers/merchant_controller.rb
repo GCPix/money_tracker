5cc92edbac0ac21224d1aab6d9c7205ce1d30cb2
GillianC <gccc0309@gmail.com>
2018-09-29 20:06:06 +0100

    functionality complete for merchant

--- controllers/merchant_controller.rb
+++ controllers/merchant_controller.rb
@@ -12,23 +12,39 @@ get '/merchants' do
   @merchant_list = Merchant.usage_frequency
   erb(:"merchants/index")
 end
+get '/merchants/new' do
 
+  erb(:"merchants/new")
+end
 get '/merchants/:id' do
   @merchant = Merchant.find(params[:id])
   erb(:"merchants/show_merchant")
 end
-
+get '/merchants/:id/edit' do
+  @merchant = Merchant.find(params[:id])
+  erb(:"merchants/edit")
+end
 get '/merchants/:id/transactions' do
   @transaction_list = Merchant.all_transactions(params[:id])
   erb(:"merchants/merchant_transactions")
 end
 
 
-
+post '/merchants' do
+  @merchant = Merchant.new(params)
+  @merchant.save
+  redirect "/merchants"
+end
 
 post '/merchants/delete/:id' do
   merchant = Merchant.find(params[:id])
 
   merchant.delete_one
   redirect "/"
+end
+
+post '/merchants/:id' do
+  @merchant = Merchant.new(params)
+  @merchant.edit()
+  redirect "/merchants"
 end
